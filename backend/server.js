a2eab3bb0b1638c84a3bac827a76834ac930a150
Oscar Andres Portillo <oscarportillo@meta.com>
2025-06-25 12:31:18 -0700

    FEATURE - User Auth - Added user authentication, register, login, logout, and session cookies

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,28 @@
 const express = require('express')
+const session = require('express-session')
 const app = express()
 const PORT = 3000
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+app.use(express.json())
 
-// Error Handling
+const auth_routes = require('./routes/user_auth')
+
+app.use(session({
+    secret: 'second-bite',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, httpOnly: true, maxAge: 1000 * 60 * 60 }
+}))
+
+app.use('/auth', auth_routes)
+
+// Error Handling Middleware
 app.use((err, req, res, next) => {
     const { message, status = 500 } = err
-    res.status(status).json({message: 'Error: ', message})
+    console.log(message)
+    res.status(status).json({message: 'Error: ' + message})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
 })
