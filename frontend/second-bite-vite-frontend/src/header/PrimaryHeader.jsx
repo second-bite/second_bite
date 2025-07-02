f394aed7443bb2fcdc037df80e7bb1949cc09361
Oscar Andres Portillo <oscarportillo@meta.com>
2025-07-02 16:18:42 -0700

    Integrated logout functionality with the backend

--- frontend/second-bite-vite-frontend/src/header/PrimaryHeader.jsx
+++ frontend/second-bite-vite-frontend/src/header/PrimaryHeader.jsx
@@ -4,16 +4,37 @@ import { Bars3Icon } from '@heroicons/react/24/solid'
 import { useNavigate } from 'react-router-dom'
 
 import { AppContext } from '../context/AppContext'
+import { AuthContext } from '../context/AuthContext'
 
 const PrimaryHeader = () => {
     const navigate = useNavigate()
 
-    const {is_feedback_modal, setIsFeedbackModal} = useContext(AppContext)
+    const {base_url, is_feedback_modal, setIsFeedbackModal} = useContext(AppContext)
+    const {setIsLoading} = useContext(AuthContext)
 
     const handleFeedbackClick = () => {setIsFeedbackModal(true)}
 
     const handleAccountInfoClick = () => {
         navigate('/account')
+    }
+    const handleSignOut = async () => {
+        try {
+            setIsLoading(true)
+            const response = await fetch(base_url + '/auth/logout', {
+                method: 'POST',
+                credentials: 'include',
+            })
+            const res_json = await response.json()
+            if(!response.ok) {
+                throw new Error(`Status Code: ${response.status}. ErrMsg: ${res_json.message}`)
+            }
+            console.log(res_json)
+            navigate('/auth')
+        } catch (e) {
+            // TODO: Meaningfully handle this error
+        } finally {
+            setIsLoading(false)
+        }
     }
 
     return (
@@ -69,6 +90,7 @@ const PrimaryHeader = () => {
                     {({ active }) => (
                         <a
                         href="#"
+                        onClick={handleSignOut}
                         className={`block px-4 py-2 text-sm ${
                             active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
                         }`}
--- frontend/second-bite-vite-frontend/src/protected_routing/ConsumerProtected.jsx
+++ frontend/second-bite-vite-frontend/src/protected_routing/ConsumerProtected.jsx
@@ -1,6 +1,7 @@
 import React, {useContext} from "react"
 import { Navigate, Outlet } from "react-router-dom"
 import { AuthContext } from "../context/AuthContext"
+import { FadeLoader } from 'react-spinners'
 
 
 const ConsumerProtected = () => {
