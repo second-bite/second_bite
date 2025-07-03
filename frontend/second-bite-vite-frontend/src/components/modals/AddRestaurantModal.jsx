8e99490b00f36b00ae4683d71ad66741a066d999
Oscar Andres Portillo <oscarportillo@meta.com>
2025-07-03 13:33:59 -0700

    Added restaurant entry validation utilities, adding frontend and backend integration, add some error messaging

--- frontend/second-bite-vite-frontend/src/components/modals/AddRestaurantModal.jsx
+++ frontend/second-bite-vite-frontend/src/components/modals/AddRestaurantModal.jsx
@@ -191,7 +191,7 @@ const AddRestaurantModal = () => {
 
           <div className="space-y-12">
             <div className="border-b border-gray-900/10 pb-12">
-              <h2 className="text-base/7 font-semibold text-gray-900">Profile</h2>
+              <h2 className="text-base/7 font-semibold text-gray-900">Restaurant Profile</h2>
               <p className="mt-1 text-sm/6 text-gray-600">
                 This information will be displayed publicly so be careful what you share.
               </p>
