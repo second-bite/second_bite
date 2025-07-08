f5502abb3f89fb59f8b9417e363b43e5c9f4c3ce
Oscar Andres Portillo <oscarportillo@meta.com>
2025-07-08 11:29:07 -0700

    Added friend icon to header (so that friend modal pops up)

--- frontend/second-bite-vite-frontend/src/header/PrimaryHeader.jsx
+++ frontend/second-bite-vite-frontend/src/header/PrimaryHeader.jsx
@@ -47,105 +47,113 @@ const PrimaryHeader = () => {
             setIsLoading(false)
         }
     }
+    const handleFriendClick = () => {
+
+    }
 
     return (
         <header className="primary_header">
             <h1>SecondBite</h1>
             <section className="account">
             <div className="relative inline-block text-left">
-            <Menu>
-                <Menu.Button className="inline-flex justify-center w-full rounded-md px-2 py-2 text-sm font-medium text-gray-500 hover:text-gray-700 focus:outline-none">
-                <Bars3Icon className="h-6 w-6 text-gray-700" />
-                </Menu.Button>
+            <section className="header_btns">
+                {
+                    (auth_status === AUTH_STATUS.CONSUMER_AUTH) ? <button type="button" className="friend_btn" onClick={handleFriendClick}><img className="friend_img" src="/friends.png" alt="Friend Icon" /></button> : null
+                }
+                <Menu>
+                    <Menu.Button className="inline-flex justify-center w-full rounded-md px-2 py-2 text-sm font-medium text-gray-500 hover:text-gray-700 focus:outline-none">
+                    <Bars3Icon className="h-6 w-6 text-gray-700" />
+                    </Menu.Button>
 
-                <Menu.Items className="absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none z-10">
-                <div className="py-1">
-                    <Menu.Item>
-                    {({ active }) => (
-                        <a
-                        href="#"
-                        onClick={handleAccountInfoClick}
-                        className={`block px-4 py-2 text-sm ${
-                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
-                        }`}
-                        >
-                        Account Details
-                        </a>
-                    )}
-                    </Menu.Item>
-                    {
-                        (auth_status === AUTH_STATUS.CONSUMER_AUTH) ? (
-                            <>
+                    <Menu.Items className="absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none z-10">
+                    <div className="py-1">
+                        <Menu.Item>
+                        {({ active }) => (
+                            <a
+                            href="#"
+                            onClick={handleAccountInfoClick}
+                            className={`block px-4 py-2 text-sm ${
+                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
+                            }`}
+                            >
+                            Account Details
+                            </a>
+                        )}
+                        </Menu.Item>
+                        {
+                            (auth_status === AUTH_STATUS.CONSUMER_AUTH) ? (
+                                <>
+                                    <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                        href="#"
+                                        className={`block px-4 py-2 text-sm ${
+                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
+                                        }`}
+                                        >
+                                        Favorited
+                                        </a>
+                                    )}
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                        href="#"
+                                        className={`block px-4 py-2 text-sm ${
+                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
+                                        }`}
+                                        >
+                                        Shopped
+                                        </a>
+                                    )}
+                                    </Menu.Item>
+                                </>
+                            ) : (auth_status === AUTH_STATUS.OWNER_AUTH) ? (
                                 <Menu.Item>
                                 {({ active }) => (
                                     <a
                                     href="#"
+                                    onClick={handleAnalyticsClick}
                                     className={`block px-4 py-2 text-sm ${
                                         active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
                                     }`}
                                     >
-                                    Favorited
+                                    Analytics
                                     </a>
                                 )}
                                 </Menu.Item>
-                                <Menu.Item>
-                                {({ active }) => (
-                                    <a
-                                    href="#"
-                                    className={`block px-4 py-2 text-sm ${
-                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
-                                    }`}
-                                    >
-                                    Shopped
-                                    </a>
-                                )}
-                                </Menu.Item>
-                            </>
-                        ) : (auth_status === AUTH_STATUS.OWNER_AUTH) ? (
-                            <Menu.Item>
-                            {({ active }) => (
-                                <a
-                                href="#"
-                                onClick={handleAnalyticsClick}
-                                className={`block px-4 py-2 text-sm ${
-                                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
-                                }`}
-                                >
-                                Analytics
-                                </a>
-                            )}
-                            </Menu.Item>
-                        ) : null
-                    }
-                    <Menu.Item>
-                    {({ active }) => (
-                        <a
-                        href="#"
-                        onClick={handleSignOut}
-                        className={`block px-4 py-2 text-sm ${
-                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
-                        }`}
-                        >
-                        Sign out
-                        </a>
-                    )}
-                    </Menu.Item>
-                    <Menu.Item>
-                    {({ active }) => (
-                        <a
-                        href="#"
-                        onClick={e => { e.preventDefault(); handleFeedbackClick(); }}
-                        className={`block px-4 py-2 text-sm ${
-                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
-                        }`}
-                        >
-                        Feedback
-                        </a>
-                    )}
-                    </Menu.Item>
-                </div>
-                </Menu.Items>
-            </Menu>
+                            ) : null
+                        }
+                        <Menu.Item>
+                        {({ active }) => (
+                            <a
+                            href="#"
+                            onClick={handleSignOut}
+                            className={`block px-4 py-2 text-sm ${
+                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
+                            }`}
+                            >
+                            Sign out
+                            </a>
+                        )}
+                        </Menu.Item>
+                        <Menu.Item>
+                        {({ active }) => (
+                            <a
+                            href="#"
+                            onClick={e => { e.preventDefault(); handleFeedbackClick(); }}
+                            className={`block px-4 py-2 text-sm ${
+                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
+                            }`}
+                            >
+                            Feedback
+                            </a>
+                        )}
+                        </Menu.Item>
+                    </div>
+                    </Menu.Items>
+                </Menu>                
+            </section>
             </div>
             </section>
         </header>
