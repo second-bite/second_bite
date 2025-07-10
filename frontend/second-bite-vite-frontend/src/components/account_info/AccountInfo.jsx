3b333bcab1d9abe75ed323f24f01ecb366b7f421
Oscar Andres Portillo <oscarportillo@meta.com>
2025-07-10 00:46:28 -0700

    Integrated account info so that consumer account info auto-populates

--- frontend/second-bite-vite-frontend/src/components/account_info/AccountInfo.jsx
+++ frontend/second-bite-vite-frontend/src/components/account_info/AccountInfo.jsx
@@ -1,11 +1,26 @@
-import React, { useState, useRef } from "react"
+import React, { useState, useRef, useEffect, useContext } from "react"
 import { useNavigate } from 'react-router-dom'
 import states from "../misc/States"
+import { AppContext } from "../../context/AppContext"
 
 
 const AccountInfo = () => {
     const navigate = useNavigate()
 
+    const { base_url } = useContext(AppContext)
+
+    // State variables
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [streetAddress, setStreetAddress] = useState('')
+    const [city, setCity] = useState('')
+    const [postal_code, setPostalCode] = useState('')
+    const [state, setState] = useState('')
+    const [country, setCountry] = useState('')
+    const [server_error_msg, setServerErrorMsg] = useState('')
+
+    // Missing fields error messages
     const [username_msg, setUsernameMsg] = useState('')
     const [password_msg, setPasswordMsg] = useState('')
     const [confirm_password_msg, setConfirmPasswordMsg] = useState('')
@@ -16,7 +31,37 @@ const AccountInfo = () => {
     const [country_msg, setCountryMsg] = useState('')
 
     const form_ref = useRef();
-    // TODO: Pre-populate with existing account info
+
+    const getConsumerInfo = async () => {
+        // Fetch owner info from DB
+        let data
+        try {
+            const response = await fetch(base_url + '/consumer', {
+                method: 'GET',
+                credentials: 'include',
+            })
+            const err = new Error(`Status: ${response.status}. Failed to retrieve owner info from DB`)
+            err.status = response.status
+            if(!response.ok) throw err
+            data = await response.json()
+        } catch (err) {
+            await log_error(err)
+        }
+        // Set state variables
+        setUsername(data.username)
+        setPassword(data.password)
+        setConfirmPassword(data.password)
+        setStreetAddress(data.address.street_address)
+        setCity(data.address.city)
+        setPostalCode(data.address.postal_code)
+        setState(data.address.state)
+        setCountry(data.address.country)
+        setOwnedRestaurants(data.restaurants)
+    }
+
+    useEffect(() => {
+        getConsumerInfo()
+    }, [])
 
     // Handlers
     const handleAccountReturn = () => {
@@ -35,33 +80,33 @@ const AccountInfo = () => {
                     {/* Username, Password, Re-Enter Password */}
                     <section className="account_entry">
                         <p className="auth_text">Username:</p>
-                        <input type="text" name="consumer_edit_username" className="account_input" placeholder={username_msg} />
+                        <input type="text" name="consumer_edit_username" className="account_input" placeholder={username_msg} value={username} onChange={event => setUsername(event.target.value)} />
                     </section>
                     <section className="account_entry">
                         <p className="auth_text">Password:</p>
-                        <input type="text" name="consumer_edit_password" className="account_input" placeholder={password_msg} />
+                        <input type="password" name="consumer_edit_password" className="account_input" placeholder={password_msg} value={password} onChange={event => setPassword(event.target.value)} />
                     </section>
                     <section className="account_entry">
                         <p className="auth_text">Confirm Password:</p>
-                        <input type="text" name="consumer_edit_confirm_password" className="account_input" placeholder={confirm_password_msg} />
+                        <input type="password" name="consumer_edit_confirm_password" className="account_input" placeholder={confirm_password_msg} value={confirmPassword} onChange={event => setConfirmPassword(event.target.value)} />
                     </section>
                     {/* Location */}
                     <section className="location_account_entries">
                         <section className="location_account_entry">
                             <p className="location_auth_text">Street Address:</p>
-                            <input type="text" name="consumer_edit_street_address" className="account_input" placeholder={street_address_msg} />
+                            <input type="text" name="consumer_edit_street_address" className="account_input" placeholder={street_address_msg} value={streetAddress} onChange={event => setStreetAddress(event.target.value)} />
                         </section>
                         <section className="location_account_entry">
                             <p className="location_auth_text">City:</p>
-                            <input type="text" name="consumer_edit_city" className="account_input" placeholder={city_msg} />
+                            <input type="text" name="consumer_edit_city" className="account_input" placeholder={city_msg} value={city} onChange={event => setCity(event.target.value)} />
                         </section>
                         <section className="location_account_entry">
                             <p className="location_auth_text">Postal Code:</p>
-                            <input type="text" name="consumer_edit_postal_code" className="account_input" placeholder={postal_code_msg} />
+                            <input type="text" name="consumer_edit_postal_code" className="account_input" placeholder={postal_code_msg} value={postal_code} onChange={event => setPostalCode(event.target.value)} />
                         </section>
                         <section className="location_account_entry">
                             <p className="location_auth_text">State:</p>
-                            <select name="consumer_edit_state" className="account_input">
+                            <select name="consumer_edit_state" className="account_input" value={state} onChange={event => setState(event.target.value)} >
                                 <option value="none">{state_msg || 'Select a State:'}</option>
                                 {states.map((state) => (
                                     <option key={state.abbreviation} value={state.abbreviation}>
@@ -72,7 +117,7 @@ const AccountInfo = () => {
                         </section>
                         <section className="location_account_entry">
                             <p className="location_auth_text">Country:</p>
-                            <select name="consumer_edit_country" className="account_input">
+                            <select name="consumer_edit_country" className="account_input" value={country} onChange={event => setCountry(event.target.value)} >
                                 <option value="none">{country_msg || 'Select a Country:'}</option>
                                 <option value="US">United States</option>
                             </select>
