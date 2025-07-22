64204959aef67eab5de65a64957c86c2796328bf
Oscar Andres Portillo <oscarportillo@meta.com>
2025-07-21 19:14:35 -0700

    Added consumer & owner seed generation

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -1,5 +1,5 @@
 const prisma = require('../routes/prisma_client')
-import { faker } from '@faker-js/faker'
+import { faker } from '@faker-js/faker/locale/en_US'
 const argon2 = require('argon2')
 
 async function main() {
@@ -15,11 +15,32 @@ async function main() {
 
     // Generate Consumers
     const consumers = []
+    const consumer_username_set = new Set()
     for (let i = 0; i < NUM_CONSUMERS; i++) {
-        const password = password.
+        let is_username_already_exists = true
+        let username = null
+        while (is_username_already_exists) {
+            username = faker.internet.username
+            if(!consumer_username_set.has(username)) {
+                consumer_username_set.add(username)
+                is_username_already_exists = false
+            }
+        }
+
+        const hashed_pwd = await argon2.hash(faker.internet.password)
+
         const data = {
-            username: faker.internet.username()
-            password
+            username: username,
+            password: hashed_pwd,
+            address: {
+                create: {
+                    street_address: faker.address.streetAddress(),
+                    city:           faker.address.city(),
+                    state:          faker.address.stateAbbr(),
+                    postal_code:    faker.address.zipCode(),
+                    country:        faker.address.country()
+                }
+            }
         }
 
         const consumer = await prisma.consumer.create({
@@ -29,8 +50,43 @@ async function main() {
         consumers.push(consumer)
     }
 
-    // Generate Owners
 
+    // Generate Owners
+    const owners = []
+    const owner_username_set = new Set()
+    for (let i = 0; i < NUM_OWNERS; i++) {
+        let is_username_already_exists = true
+        let username = null
+        while (is_username_already_exists) {
+            username = faker.internet.username
+            if(!owner_username_set.has(username)) {
+                owner_username_set.add(username)
+                is_username_already_exists = false
+            }
+        }
+
+        const hashed_pwd = await argon2.hash(faker.internet.password)
+
+        const data = {
+            username: username,
+            password: hashed_pwd,
+            address: {
+                create: {
+                    street_address: faker.address.streetAddress(),
+                    city:           faker.address.city(),
+                    state:          faker.address.stateAbbr(),
+                    postal_code:    faker.address.zipCode(),
+                    country:        faker.address.country()
+                }
+            }
+        }
+
+        const owner = await prisma.owner.create({
+            data: data
+        })
+
+        owners.push(owner)
+    }
 
     // Generate Restaurants
 
