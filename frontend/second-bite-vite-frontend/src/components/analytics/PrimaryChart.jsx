5ad328bcffdcd72f788704c6ee94ac9b4fa62702
Oscar Andres Portillo <oscarportillo@meta.com>
2025-07-23 22:48:20 -0700

    UI Update: Added confidence intervals

--- frontend/second-bite-vite-frontend/src/components/analytics/PrimaryChart.jsx
+++ frontend/second-bite-vite-frontend/src/components/analytics/PrimaryChart.jsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from "react"
 import { DateTime } from 'luxon'
-import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { CartesianGrid, Legend, Line, Area, ComposedChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 
 const PrimaryChart = ({ orders, visits, kpi_time_range, KPI_TIME_RANGE }) => {
     const [graph_data, setGraphData] = useState([])
 
-    // TODO: Future prediction based on mean
-    const add_prediction_point = () => {
-
+    // TODO: Get standard deviation
+    // Credit: https://www.indeed.com/career-advice/career-development/how-to-calculate-standard-deviation
+    const get_standard_deviation = (data) => {
+        const mean = data.reduce((accumulator, val) => accumulator + val, 0) / data.length
+        const sum_squared_diff = data.reduce((accumulator, val) => accumulator + (val - mean) ** 2, 0)
+        const standard_dev = Math.sqrt(sum_squared_diff / data.length)
+        return standard_dev
     }
 
     const structureMonthlyData = () => {
@@ -51,19 +55,38 @@ const PrimaryChart = ({ orders, visits, kpi_time_range, KPI_TIME_RANGE }) => {
                 name: `Week of ${start_of_week.toFormat("MMM d")}`,
                 orders: order_count_map.get(week.key) || 0,       
                 visits: visit_count_map.get(week.key) || 0,  
+                visits_low: visit_count_map.get(week.key) || 0, 
+                visits_band: 0,
+                orders_low: order_count_map.get(week.key) || 0,  
+                orders_band: 0,
             }
         })
 
-        // TODO: Add next week prediction
+        // Add next month prediction
         const predicted_week = now.plus({ weeks: 1 })
         const start_of_predicted_week = DateTime.fromObject({
             weekYear: predicted_week.weekYear,
             weekNumber: predicted_week.weekNumber
         }).startOf("week")
+        const order_num_arr = Array.from(order_count_map.values())
+        const visit_num_arr = Array.from(visit_count_map.values())
         const predicted_name = `Week of ${start_of_predicted_week.toFormat("MMM d")}`
         const predicted_order = Array.from(order_count_map.values()).reduce((accumulator, val) => accumulator + val, 0) / 7
         const predicted_visit = Array.from(visit_count_map.values()).reduce((accumulator, val) => accumulator + val, 0) / 7
-        new_data.push({name: predicted_name, orders: predicted_order, visits: predicted_visit})
+
+        // Add confidence interval to next day prediction
+        // Credit: For confidence interval, I used: https://www.westga.edu/academics/research/vrc/assets/docs/confidence_intervals_notes.pdf
+        const target_arr_length = 5 // Need an entry per week
+        const padded_order_num_arr = order_num_arr.concat(Array(target_arr_length - order_num_arr.length).fill(0))
+        const padded_visit_num_arr = visit_num_arr.concat(Array(target_arr_length - visit_num_arr.length).fill(0))
+        const order_standard_dev = get_standard_deviation(padded_order_num_arr)
+        const visit_standard_dev = get_standard_deviation(padded_visit_num_arr)
+        const z_score = 1.96 // 95% Confidence Interval
+        const order_err_margin = z_score * order_standard_dev / Math.sqrt(padded_order_num_arr.length)
+        const visit_err_margin = z_score * visit_standard_dev / Math.sqrt(padded_visit_num_arr.length)
+        new_data.push({ name: predicted_name, orders: predicted_order, visits: predicted_visit, 
+                        visits_low: predicted_visit - visit_err_margin, visits_band: 2 * visit_err_margin,
+                        orders_low: predicted_order - order_err_margin, orders_band: 2 * order_err_margin })
 
         setGraphData(new_data)
     }
@@ -99,13 +122,32 @@ const PrimaryChart = ({ orders, visits, kpi_time_range, KPI_TIME_RANGE }) => {
             name: day.toFormat('cccc'),
             orders: order_count_map.get(day.toISODate()) || 0,
             visits: visit_count_map.get(day.toISODate()) || 0,
+            visits_low: visit_count_map.get(day.toISODate()) || 0,
+            visits_band: 0,
+            orders_low: order_count_map.get(day.toISODate()) || 0,
+            orders_band: 0,
         }))
 
-        // TODO: Add next day prediction
+        // Add next day prediction
+        const order_num_arr = Array.from(order_count_map.values())
+        const visit_num_arr = Array.from(visit_count_map.values())
         const predicted_name = now.plus({ days: 1 }).toFormat('cccc')
-        const predicted_order = Array.from(order_count_map.values()).reduce((accumulator, val) => accumulator + val, 0) / 7
-        const predicted_visit = Array.from(visit_count_map.values()).reduce((accumulator, val) => accumulator + val, 0) / 7
-        new_data.push({name: predicted_name, orders: predicted_order, visits: predicted_visit})
+        const predicted_order = order_num_arr.reduce((accumulator, val) => accumulator + val, 0) / 7
+        const predicted_visit = visit_num_arr.reduce((accumulator, val) => accumulator + val, 0) / 7
+        
+        // Add confidence interval to next day prediction
+        // Credit: For confidence interval, I used: https://www.westga.edu/academics/research/vrc/assets/docs/confidence_intervals_notes.pdf
+        const target_arr_length = 7 // Need an entry per weekday
+        const padded_order_num_arr = order_num_arr.concat(Array(target_arr_length - order_num_arr.length).fill(0))
+        const padded_visit_num_arr = visit_num_arr.concat(Array(target_arr_length - visit_num_arr.length).fill(0))
+        const order_standard_dev = get_standard_deviation(padded_order_num_arr)
+        const visit_standard_dev = get_standard_deviation(padded_visit_num_arr)
+        const z_score = 1.96 // 95% Confidence Interval
+        const order_err_margin = z_score * order_standard_dev / Math.sqrt(padded_order_num_arr.length)
+        const visit_err_margin = z_score * visit_standard_dev / Math.sqrt(padded_visit_num_arr.length)
+        new_data.push({ name: predicted_name, orders: predicted_order, visits: predicted_visit, 
+                        visits_low: predicted_visit - visit_err_margin, visits_band: 2 * visit_err_margin,
+                        orders_low: predicted_order - order_err_margin, orders_band: 2 * order_err_margin })
 
         setGraphData(new_data)
     }
@@ -123,9 +165,13 @@ const PrimaryChart = ({ orders, visits, kpi_time_range, KPI_TIME_RANGE }) => {
         <>
             <section className="analytics_graph">
               {/* Primary Chart */}
-              {/* Code largely taken from https://recharts.org/en-US/examples/BiaxialLineChart */}
+              {/* Code largely taken from:
+                - https://recharts.org/en-US/examples/BiaxialLineChart 
+                - https://recharts.org/en-US/api/AreaChart 
+                - https://recharts.org/en-US/api/ComposedChart
+                */}
               <ResponsiveContainer width="100%" height="100%">
-                <LineChart
+                <ComposedChart
                   width={500}
                   height={300}
                   data={graph_data}
@@ -142,9 +188,49 @@ const PrimaryChart = ({ orders, visits, kpi_time_range, KPI_TIME_RANGE }) => {
                   <YAxis yAxisId="right" orientation="right" />
                   <Tooltip />
                   <Legend />
-                  <Line yAxisId="left" type="monotone" dataKey="orders" stroke="#8884d8" activeDot={{ r: 8 }} />
-                  <Line yAxisId="right" type="monotone" dataKey="visits" stroke="#82ca9d" />
-                </LineChart>
+
+                  {/* Primary Lines */}
+                  <Line yAxisId="left" type="monotone" dataKey="orders" name="orders" stroke="#8884d8" activeDot={{ r: 8 }} />
+                  <Line yAxisId="right" type="monotone" dataKey="visits" name="visits" stroke="#82ca9d" />
+
+                  {/* Confidence Interval Area - Orders */}
+                  <Area
+                    yAxisId="left"
+                    type="monotone"
+                    dataKey="orders_low"
+                    stackId="orders_confidence_interval"
+                    stroke="none"
+                    fill="transparent"
+                  />
+                  <Area
+                    yAxisId="left"
+                    type="monotone"
+                    dataKey="orders_band"
+                    stackId="orders_confidence_interval"
+                    stroke="none"
+                    fill="#8884D8"
+                    fillOpacity={0.2}
+                  />
+
+                  {/* Confidence Interval Area - Visits */}
+                  <Area
+                    yAxisId="right"
+                    type="monotone"
+                    dataKey="visits_low"
+                    stackId="visits_confidence_interval"
+                    stroke="none"
+                    fill="transparent"
+                  />
+                  <Area
+                    yAxisId="right"
+                    type="monotone"
+                    dataKey="visits_band"
+                    stackId="visits_confidence_interval"
+                    stroke="none"
+                    fill="#FA8072"
+                    fillOpacity={0.2}
+                  />
+                </ComposedChart>
               </ResponsiveContainer>
             </section>
         </>
